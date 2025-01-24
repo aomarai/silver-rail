669abada053179608bcd7874a3a1f70cd13e8a05
Ash Omaraie <49348213+aomarai@users.noreply.github.com>
2025-01-23 20:47:23 -0600

    Created separate apps for teams, characters, relics, and lightcones

--- silverrail/settings.py
+++ silverrail/settings.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +36,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'characters',
+    'teams',
+    'relics',
+    'lightcones',
 ]
 
 MIDDLEWARE = [
